d22e6e8164eb62cbc22a54519dec8f358efe180f
git-leon <xleonhunter@gmail.com>
2023-09-11 17:09:57 -0400

    fleshed out solutions a little more

--- src/main/java/com/github/curriculeon/distinctdigits/Solution.java
+++ src/main/java/com/github/curriculeon/distinctdigits/Solution.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.Set;
 
 public class Solution {
-    public Integer getAmountOfNumbersWithOnlyUniqueDigits(List<List<Integer>> input) {
-        Set<Integer[]> allRecordeUniqueDigits = new HashSet<>();
-        for(List<Integer> upperAndLowerBound : input) {
-            int currentUpperBound = upperAndLowerBound.get(0);
-            int currentLowerBound = upperAndLowerBound.get(1);
-            NumberRange currentNumberRange = new NumberRange(currentUpperBound, currentLowerBound);
-            Integer[] currentUniqueDigits = currentNumberRange.getValuesWithUniqueDigits();
+    public Integer getAmountOfNumbersWithOnlyUniqueDigits(final List<List<Integer>> input) {
+        final Set<Integer[]> allRecordeUniqueDigits = new HashSet<>();
+        for(final List<Integer> upperAndLowerBound : input) {
+            final int currentUpperBound = upperAndLowerBound.get(0);
+            final int currentLowerBound = upperAndLowerBound.get(1);
+            final NumberRange currentNumberRange = new NumberRange(currentUpperBound, currentLowerBound);
+            final Integer[] currentUniqueDigits = currentNumberRange.getValuesWithUniqueDigits();
             Arrays.sort(currentUniqueDigits); // ensure any permutation of these values are in an identical order
             allRecordeUniqueDigits.add(currentUniqueDigits);
         }
--- src/main/java/com/github/curriculeon/lottery/CouponNumbers.java
+++ src/main/java/com/github/curriculeon/lottery/CouponNumbers.java
@@ -1,7 +1,8 @@
 package com.github.curriculeon.lottery;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class CouponNumbers {
@@ -9,13 +10,32 @@ public class CouponNumbers {
 
 
     public CouponNumbers(int n) {
-        this.lotteryTicketList = new ArrayList<>();
-        IntStream
+        this.lotteryTicketList = IntStream
                 .range(1, n)
-                .forEach(ticketNumber -> lotteryTicketList.add(new LotteryTicket(ticketNumber)));
+                .mapToObj(LotteryTicket::new)
+                .collect(Collectors.toList());
+    }
+
+    public Map<Integer, List<Integer>> getSumsAndRespectiveLotteryNumbers() {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        lotteryTicketList.forEach(lotteryTicket -> {
+            final int lotteryTicketValue = lotteryTicket.getTicketValue();
+            final int lotteryTicketNumber = lotteryTicket.getTicketNumber();
+            map.putIfAbsent(lotteryTicketNumber, new ArrayList<>());
+            final List<Integer> lotteryTicketValues = map.get(lotteryTicketValue);
+            lotteryTicketValues.add(lotteryTicket.getTicketNumber());
+            map.put(lotteryTicketNumber, lotteryTicketValues);
+        });
+        return map;
     }
 
     public Integer getLargestNumberOfWinners() {
-        return null; // TODO
+        List<Map.Entry<Integer, List<Integer>>> sortable = new ArrayList<>(getSumsAndRespectiveLotteryNumbers().entrySet());
+        sortable.sort((entry1, entry2) -> {
+            int size1 = entry1.getValue().size();
+            int size2 = entry2.getValue().size();
+            return Integer.compare(size1, size2);
+        });
+        return sortable.get(0).getValue().size();
     }
 }
