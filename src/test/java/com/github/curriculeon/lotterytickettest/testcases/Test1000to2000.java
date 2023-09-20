ff76c5cac2f4958eb6f6823f26cdd4539b31441b
git-leon <xleonhunter@gmail.com>
2023-09-20 03:41:26 -0400

    updated tests up to 1000

---
+++ src/test/java/com/github/curriculeon/lotterytickettest/testcases/Test1000to2000.java
@@ -0,0 +1,20 @@
+package com.github.curriculeon.lotterytickettest.testcases;
+
+import com.github.curriculeon.lotterytickettest.LotteryTicketTestInterface;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface Test1000to2000 extends LotteryTicketTestInterface {
+    @Test
+    default void test1000to2000() {
+        // ten 1s expected in the 90s
+        List<Integer> exceptions = Arrays.asList(1038, 1039);
+        for (int i = 1000; i < 2000; i++) {
+            if (!exceptions.contains(i)) {
+                test(i, 2);
+            }
+        }
+    }
+}
